f457006ac0ac4888342e3d0fef2fc7aa426e45d1
SimoneZoroddu <lenviur@gmail.com>
2026-03-26 14:06:31 +0100

    milestone 3 stars

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -41,24 +41,24 @@ export default function Main() {
                     <div key={film.id}>
                         <h1>{film.title}</h1>
                         <h2>{film.original_title}</h2>
-                        <div>{Math.ceil(film.vote_average / 2)}</div>
+                        <div>{'★'.repeat(Math.ceil(film.vote_average / 2))}{'☆'.repeat(5 - (Math.ceil(film.vote_average / 2)))}</div>
                         <div className={`fi fi-${film.original_language === "en" ? "gb" : film.original_language === "ja" ? "jp" : film.original_language === "zh" ? "kr" : film.original_language}`}></div>
-                        <img src={ film.poster_path ? `https://image.tmdb.org/t/p/w342/${film.poster_path}` : 'https://image.tmdb.org/t/p/w342/wwemzKWzjKYJFfCeiB57q3r4Bcm.png'} alt="" />
+                        <img src={film.poster_path ? `https://image.tmdb.org/t/p/w342/${film.poster_path}` : 'https://image.tmdb.org/t/p/w342/wwemzKWzjKYJFfCeiB57q3r4Bcm.png'} alt="" />
                     </div >
-
-                ))
+                )
+                )
             }
             {
                 serieTVSearced.map((serieTv) => (
                     <div key={serieTv.id}>
                         <h1>{serieTv.name}</h1>
                         <h2>{serieTv.original_name}</h2>
-                        <div>{Math.ceil(serieTv.vote_average / 2)}</div>
+                        <div>{'★'.repeat(Math.ceil(serieTv.vote_average / 2))}{'☆'.repeat(5 - (Math.ceil(serieTv.vote_average / 2)))}</div>
                         <div className={`fi fi-${serieTv.original_language === "en" ? "gb" : serieTv.original_language === "ja" ? "jp" : serieTv.original_language === "zh" ? "kr" : serieTv.original_language}`}></div>
-                        <img src={ serieTv.poster_path ? `https://image.tmdb.org/t/p/w342/${serieTv.poster_path}` : 'https://image.tmdb.org/t/p/w342/wwemzKWzjKYJFfCeiB57q3r4Bcm.png' }  alt="" />
+                        <img src={serieTv.poster_path ? `https://image.tmdb.org/t/p/w342/${serieTv.poster_path}` : 'https://image.tmdb.org/t/p/w342/wwemzKWzjKYJFfCeiB57q3r4Bcm.png'} alt="" />
                     </div >
-
-                ))
+                )
+                )
             }
         </>
 
