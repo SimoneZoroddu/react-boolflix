c237e7c24cd98091b82deb8b039313c73d9bbf2b
SimoneZoroddu <lenviur@gmail.com>
2026-03-26 12:34:31 +0100

    adding img at the page

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -43,17 +43,19 @@ export default function Main() {
                         <h2>{film.original_title}</h2>
                         <div>{film.vote_average}</div>
                         <div className={`fi fi-${film.original_language === "en" ? "gb" : film.original_language === "ja" ? "jp" : film.original_language === "zh" ? "kr" : film.original_language}`}></div>
+                        <img src={`https://image.tmdb.org/t/p/w342/${film.poster_path}`} alt="" />
                     </div >
 
                 ))
             }
             {
-                serieTVSearced.map((film) => (
-                    <div key={film.id}>
-                        <h1>{film.name}</h1>
-                        <h2>{film.original_name}</h2>
-                        <div>{film.vote_average}</div>
-                        <div className={`fi fi-${film.original_language === "en" ? "gb" : film.original_language === "ja" ? "jp" : film.original_language === "zh" ? "kr" : film.original_language}`}></div>
+                serieTVSearced.map((serieTv) => (
+                    <div key={serieTv.id}>
+                        <h1>{serieTv.name}</h1>
+                        <h2>{serieTv.original_name}</h2>
+                        <div>{serieTv.vote_average}</div>
+                        <div className={`fi fi-${serieTv.original_language === "en" ? "gb" : serieTv.original_language === "ja" ? "jp" : serieTv.original_language === "zh" ? "kr" : serieTv.original_language}`}></div>
+                        <img src={`https://image.tmdb.org/t/p/w342/${serieTv.poster_path}`}  alt="" />
                     </div >
 
                 ))
