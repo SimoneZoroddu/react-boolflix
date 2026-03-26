6ba88e5b66af94ec9ed739247245c9f54f3c9711
SimoneZoroddu <lenviur@gmail.com>
2026-03-26 15:55:10 +0100

    adjust with bootstrap aesthetic

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,4 +1,4 @@
-import { useCardContext }from '../context/CardContext'
+import { useCardContext } from '../context/CardContext'
 
 
 export default function AppHeader() {
@@ -7,11 +7,12 @@ export default function AppHeader() {
 
 
   return (
-    <>
-      <form action="" onSubmit={searchMovies}>
-        <input type="text" value={valueSearced} onChange={(e) => setValueSearced(e.target.value)} />
-        <button>Cerca il film</button>
+    <div className='bg-black d-flex justify-content-between align-items-center'>
+      <img src="https://image.tmdb.org/t/p/w342/wwemzKWzjKYJFfCeiB57q3r4Bcm.png" alt="Logo Boolfix" className='m-3 d-none d-md-block' />
+      <form action="" onSubmit={searchMovies} className='d-flex justify-content-center align-items-center flex-wrap text-center'>
+        <input placeholder='Scrivi il titolo del film' type="text" value={valueSearced} onChange={(e) => setValueSearced(e.target.value)} className='m-3 rounded bg-black text-white text-center' />
+        <button className='btn btn-secondary m-3'>Cerca un film</button>
       </form>
-    </>
+    </div>
   )
 }
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,38 +1,49 @@
-import { useCardContext }from '../context/CardContext'
+import { useCardContext } from '../context/CardContext'
 
 export default function Main() {
 
-    const {filmsSearced, serieTVSearced} = useCardContext()
-    
+    const { filmsSearced, serieTVSearced } = useCardContext()
+
 
     return (
-        <>
-            
-            {
-                filmsSearced.map((film) => (
-                    <div key={film.id}>
-                        <h1>{film.title}</h1>
-                        <h2>{film.original_title}</h2>
-                        <div>{'★'.repeat(Math.ceil(film.vote_average / 2))}{'☆'.repeat(5 - (Math.ceil(film.vote_average / 2)))}</div>
-                        <div className={`fi fi-${film.original_language === "en" ? "gb" : film.original_language === "ja" ? "jp" : film.original_language === "zh" ? "kr" : film.original_language}`}></div>
-                        <img src={film.poster_path ? `https://image.tmdb.org/t/p/w342/${film.poster_path}` : 'https://image.tmdb.org/t/p/w342/wwemzKWzjKYJFfCeiB57q3r4Bcm.png'} alt={film.original_title} />
-                    </div >
-                )
-                )
-            }
-            {
-                serieTVSearced.map((serieTv) => (
-                    <div key={serieTv.id}>
-                        <h1>{serieTv.name}</h1>
-                        <h2>{serieTv.original_name}</h2>
-                        <div>{'★'.repeat(Math.ceil(serieTv.vote_average / 2))}{'☆'.repeat(5 - (Math.ceil(serieTv.vote_average / 2)))}</div>
-                        <div className={`fi fi-${serieTv.original_language === "en" ? "gb" : serieTv.original_language === "ja" ? "jp" : serieTv.original_language === "zh" ? "kr" : serieTv.original_language}`}></div>
-                        <img src={serieTv.poster_path ? `https://image.tmdb.org/t/p/w342/${serieTv.poster_path}` : 'https://image.tmdb.org/t/p/w342/4SZQqcsaETKSKo15lYQO8RAcBpv.jpg'} alt={serieTv.original_name} />
-                    </div >
-                )
-                )
-            }
-        </>
+        <div className='bg-secondary'>
+            <div className="container">
+                <div className="row row-cols-1 row-cols-md-3 row-cols-lg-5">
+                    {
+                        filmsSearced.map((film) => (
+                            <div className="col p-3" key={film.id}>
+                                <img src={film.poster_path ? `https://image.tmdb.org/t/p/w342/${film.poster_path}` : 'https://image.tmdb.org/t/p/w342/wwemzKWzjKYJFfCeiB57q3r4Bcm.png'} alt={film.original_title} className='img-fluid'/>
+                                <div className='d-none'>
+                                    <h1><strong>Titolo</strong>:{film.title}</h1>
+                                    <h2><strong>Titolo originale</strong>:{film.original_title}</h2>
+                                    <div><strong>Voto</strong>:{'★'.repeat(Math.ceil(film.vote_average / 2))}{'☆'.repeat(5 - (Math.ceil(film.vote_average / 2)))}</div>
+                                    <div className={`fi fi-${film.original_language === "en" ? "gb" : film.original_language === "ja" ? "jp" : film.original_language === "zh" ? "kr" : film.original_language}`}></div>
+                                    <div><strong>Overview</strong>:{film.overview}</div>
+                                </div>
+                            </div>
+
+                        )
+                        )
+                    }
+                    {
+                        serieTVSearced.map((serieTv) => (
+                            <div className="col p-3" key={serieTv.id}>
+                                <img src={serieTv.poster_path ? `https://image.tmdb.org/t/p/w342/${serieTv.poster_path}` : 'https://image.tmdb.org/t/p/w342/4SZQqcsaETKSKo15lYQO8RAcBpv.jpg'} alt={serieTv.original_name} className='img-fluid' />
+                                <div className='d-none'>
+                                    <h1>{serieTv.name}</h1>
+                                    <h2>{serieTv.original_name}</h2>
+                                    <div>{'★'.repeat(Math.ceil(serieTv.vote_average / 2))}{'☆'.repeat(5 - (Math.ceil(serieTv.vote_average / 2)))}</div>
+                                    <div className={`fi fi-${serieTv.original_language === "en" ? "gb" : serieTv.original_language === "ja" ? "jp" : serieTv.original_language === "zh" ? "kr" : serieTv.original_language}`}></div>
+                                    <div><strong>Overview</strong>:{serieTv.overview}</div>
+                                </div>
+                            </div >
+                        )
+                        )
+                    }
+
+                </div>
+            </div>
+        </div>
 
     )
 }
