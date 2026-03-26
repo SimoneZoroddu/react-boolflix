db443f558113651a4dc879ca1edd43d537828361
SimoneZoroddu <lenviur@gmail.com>
2026-03-26 12:52:08 +0100

    vote in number 0 to 5 with excess

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -41,7 +41,7 @@ export default function Main() {
                     <div key={film.id}>
                         <h1>{film.title}</h1>
                         <h2>{film.original_title}</h2>
-                        <div>{film.vote_average}</div>
+                        <div>{Math.ceil(film.vote_average / 2)}</div>
                         <div className={`fi fi-${film.original_language === "en" ? "gb" : film.original_language === "ja" ? "jp" : film.original_language === "zh" ? "kr" : film.original_language}`}></div>
                         <img src={ film.poster_path ? `https://image.tmdb.org/t/p/w342/${film.poster_path}` : 'https://image.tmdb.org/t/p/w342/wwemzKWzjKYJFfCeiB57q3r4Bcm.png'} alt="" />
                     </div >
@@ -53,7 +53,7 @@ export default function Main() {
                     <div key={serieTv.id}>
                         <h1>{serieTv.name}</h1>
                         <h2>{serieTv.original_name}</h2>
-                        <div>{serieTv.vote_average}</div>
+                        <div>{Math.ceil(serieTv.vote_average / 2)}</div>
                         <div className={`fi fi-${serieTv.original_language === "en" ? "gb" : serieTv.original_language === "ja" ? "jp" : serieTv.original_language === "zh" ? "kr" : serieTv.original_language}`}></div>
                         <img src={ serieTv.poster_path ? `https://image.tmdb.org/t/p/w342/${serieTv.poster_path}` : 'https://image.tmdb.org/t/p/w342/wwemzKWzjKYJFfCeiB57q3r4Bcm.png' }  alt="" />
                     </div >
